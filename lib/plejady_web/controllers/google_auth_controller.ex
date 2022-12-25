a47513a94e548903b96772623c1f179a63290d7f
Martin Rosenberg <nitram.rosenberg@gmail.com>
2022-12-25 12:46:14 +0100

    💄 Switched all styles to SCSS

--- lib/plejady_web/controllers/google_auth_controller.ex
+++ lib/plejady_web/controllers/google_auth_controller.ex
@@ -37,7 +37,7 @@ defmodule PlejadyWeb.GoogleAuthController do
 
   def log_out(conn, _params) do
     conn
-    |> put_flash(:info, "Logged out successfully.")
+    |> put_flash(:info, "Odhlášení proběhlo úspěšně.")
     |> UserAuth.log_out_user()
   end
 end
